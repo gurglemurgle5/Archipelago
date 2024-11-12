27f802d3391f3f985ebccc70e33726f531b4ca4a
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2024-11-12 17:24:56 -0600

    Some minor cleanup

--- worlds/oneshot_wme/__init__.py
+++ worlds/oneshot_wme/__init__.py
@@ -425,36 +425,3 @@ class ManualWorld(World):
             'regions': region_table,
             'categories': category_table
         }
-
-###
-# Non-world client methods
-###
-
-def launch_client(*args):
-    import CommonClient
-    from .ManualClient import launch as Main
-
-    if CommonClient.gui_enabled:
-        launch_subprocess(Main, name="Manual client")
-    else:
-        Main()
-
-class VersionedComponent(Component):
-    def __init__(self, display_name: str, script_name: Optional[str] = None, func: Optional[Callable] = None, version: int = 0, file_identifier: Optional[Callable[[str], bool]] = None):
-        super().__init__(display_name=display_name, script_name=script_name, func=func, component_type=Type.CLIENT, file_identifier=file_identifier)
-        self.version = version
-
-def add_client_to_launcher() -> None:
-    version = 2024_09_29 # YYYYMMDD
-    found = False
-    for c in components:
-        if c.display_name == "Manual Client":
-            found = True
-            if getattr(c, "version", 0) < version:  # We have a newer version of the Manual Client than the one the last apworld added
-                c.version = version
-                c.func = launch_client
-                return
-    if not found:
-        components.append(VersionedComponent("Manual Client", "ManualClient", func=launch_client, version=version, file_identifier=SuffixIdentifier('.apmanual')))
-
-add_client_to_launcher()
